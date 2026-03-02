5017df6d0795d565b5b7a964622aa8478e3aa4d4
Ammah <anjitta01@gmail.com>
2026-03-02 14:47:06 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabdul IT Solutions</title>
+</head>
+<body>
+    <h1>FABDUL IT SOLUTIONS</h1>
+</body>
+</html>
